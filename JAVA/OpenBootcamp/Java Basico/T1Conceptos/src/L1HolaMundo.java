61a72bf409e9e690cf21019593801c8750b4b4c4
JulioGaitero <89090542+JulioGaitero@users.noreply.github.com>
2023-01-23 09:13:45 +0100

    Delete JAVA/OpenBootcamp directory

--- JAVA/OpenBootcamp/Java Basico/T1Conceptos/src/L1HolaMundo.java
+++
@@ -1,9 +0,0 @@
-public class L1HolaMundo {
-
-    public static void main(String[] args){
-
-        System.out.println("Hola mundo");
-
-    }
-
-}
--- JAVA/OpenBootcamp/Java Basico/T1Conceptos/src/L3VariablesTipos.java
+++
@@ -1,7 +0,0 @@
-public class L3VariablesTipos {
-
-    public static void main(String[] args) {
-        
-    }
-
-}
